43adb77db791cbea14e8599d3c97d92b4b9423f2
Christian_Bennett <christian_d_bennett@yahoo.com>
2020-02-26 11:26:08 -0600

    add csharp directory

---
+++ 1_csharp/Digimon/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Digimon
+{
+
+    public class Digimon
+    {}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.Fight(null, null);
+        }
+
+        public void Fight(Digimon a, Digimon b)
+        {
+
+            System.Console.WriteLine("{0} fights {1}", a, b);
+
+        }
+    }
+}
---
+++ 1_csharp/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int x = 10;
+	    int y = x;
+	    x = 20;
+	    Console.WriteLine(y);
+        }
+    }
+}
