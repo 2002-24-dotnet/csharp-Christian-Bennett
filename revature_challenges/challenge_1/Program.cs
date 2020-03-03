022e241d88620d4b42e2e801a28bf11e9340b1df
Christian_Bennett <christian_d_bennett@yahoo.com>
2020-03-03 10:28:34 -0600

    First

---
+++ revature_challenges/challenge_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace challenge_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "race car";
+            Regex.Replace(str, @"\s+", "");
+
+            char[] arr = str.ToCharArray();
+
+            Array.Reverse(arr);
+
+            Console.WriteLine(new string(arr) == str);
+        }
+    }
+}
